4ad81aea92100e07f1b5ebe6c7504c07e0256914
Peter Keel <git@discordia.ch>
2020-05-07 00:35:41 +0200

    batch-unsharp-mask.sh: shellcheck'ed

--- batch-unsharp-mask.sh
+++ batch-unsharp-mask.sh
@@ -8,7 +8,7 @@ if [ $# -lt 1 ]
 then
     ARGS='*.jpg'
 else 
-    ARGS="$@"
+    ARGS="$*"
 fi
-echo Adjusting/Converting ${ARGS}
+echo Adjusting/Converting "${ARGS}"
 gimp -i --batch-interpreter='plug-in-script-fu-eval' -b "(batch-unsharp-mask \"${ARGS}\" 5.0 0.5 0)(gimp-quit 0)"
